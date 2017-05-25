228ee023d96ef8e93eb60865e7b9dede69d645e6
Estudiante <you@example.com>
2017-05-25 09:34:07 -0500

    Añadiendo funcion para presentar distancia

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include "point.h"
 
-int main(){
+//===============================
+//  Declaracion de funciones
+//===============================
+void mostrarMensajeInicial();
+void mostrarDistancia(float distancia);
 
+
+int main(){
+    
 
     return 0;
+}
+
+
+//===============================
+//  Implementacion de funciones
+//===============================
+void mostrarMensajeInicial(){
+
+}
+
+void mostrarDistancia(float distancia){
+    printf("\n--------------------------------");
+    printf("\nLa distancia entre los puntos:");
+    printf("\n  A (x,y,z)");
+    printf("\n  B (x,y,z)");
+    printf("\n... es: %f [u]", distancia);
+    printf("\n--------------------------------");
 }
