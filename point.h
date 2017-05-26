da9661be97364cb3aa8b6f6893a30829a15a9a07
ELiza Sanchez <iza_707@hotmail.com>
2017-05-25 21:19:13 -0500

    Funcion calcularDistancia

--- point.h
+++ point.h
@@ -17,3 +17,14 @@ point crearPunto(int x, int y, int z){
 	p.coorZ = z;
 	return p;
 }
+
+float calcularDistancia(point p1, point p2){
+	float dist, dx, dy, dz;
+
+	dx = p1.coorX - p2.coorX;
+	dy = p1.coorY - p2.coorY;
+	dz = p1.coorZ - p2.coorZ;
+
+	dist = sqrt(dx * dx + dy * dy + dz * dz);
+	return dist;
+}
